3dda89be87ff101007865a3e53bdc979aebc5688
Phxtanes_ <121931713+Phxtanes@users.noreply.github.com>
2025-10-07 21:03:19 +0700

    edited

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Phutaness_Portfolio/',
+  base: '/Phutanes_Portfolio/', 
 })
